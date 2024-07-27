2cc35d540be80317c8d68bfe9f709945309cae12
Arsath-2004 <getharsath@gmail.com>
2024-07-27 15:11:09 +0530

    updated code

--- api.py
+++ api.py
@@ -51,3 +51,10 @@ async def generate_cover_letter(file: UploadFile = File(...)):
     response = tokenizer.decode(outputs[0], skip_special_tokens=True)
     
     return {"coverLetter": response}
+
+
+if __name__ == "__main__":
+    public_url = ngrok.connect(5000)
+    print(f"Public URL: {public_url}")
+
+    uvicorn.run(app, host="0.0.0.0", port=5000)
