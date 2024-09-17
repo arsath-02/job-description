f98ab128ff1ee176603f52b1bdc5d2643354d701
Arsath mohamed <165041940+arsath-02@users.noreply.github.com>
2024-09-18 00:54:43 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
       <BrowserRouter>
         <Routes>   
           <Route path='/' element={<Home />} />
-          >
+          
           <Route path='*' element={<h1>Not Found</h1>} />
         </Routes>
       </BrowserRouter>    
